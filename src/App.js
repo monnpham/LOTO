3b3555deaaf6e7309acd246bb480c4996761625d
Mon <monnpham19@gmail.com>
2024-01-23 18:45:08 +0700

    A

--- src/App.js
+++ src/App.js
@@ -4,58 +4,6 @@ import Home from './page/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './page/HomePage';
 
-class RandomBoard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      board: this.generateRandomBoard()
-    };
-  }
-
-  generateUniqueNumbers(start, end, count) {
-    const numbers = [];
-    while (numbers.length < count) {
-      const num = Math.floor(Math.random() * (end - start + 1)) + start;
-      if (!numbers.includes(num)) {
-        numbers.push(num);
-      }
-    }
-    return numbers;
-  }
-
-  generateRandomBoard() {
-    const ranges = [
-      [1, 15],
-      [16, 30],
-      [31, 45],
-      [46, 60],
-      [61, 75]
-    ];
-
-    const board = ranges.map(range => {
-      return this.generateUniqueNumbers(range[0], range[1], 5);
-    });
-
-    return board;
-  }
-
-  render() {
-    return (
-      <div className="board-container">
-        {this.state.board.map((row, rowIndex) => (
-          <div key={rowIndex} className="board-column">
-            {row.map((number, colIndex) => (
-              <div key={colIndex} className="board-cell">
-                {number}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
-
 function App() {
 
   return (
